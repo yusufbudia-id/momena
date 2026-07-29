ed4a17770b7fda2fc10b0011d5eff403857494b9
Yusuf Budi Abdullah <budiabdullahy@gmail.com>
2026-07-29 19:47:22 +0700

    Initial commit

---
+++ src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function SettingsPage() {
+  return (
+    <div className="space-y-2">
+      <h1 className="text-2xl font-bold">Settings</h1>
+      <p>Coming soon.</p>
+    </div>
+  );
+}
